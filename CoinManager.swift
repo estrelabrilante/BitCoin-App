71af1b07618b78f6b39db1c42deb6093b786d94a
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-07-13 09:00:25 -0400

    parseJSON into swift object

--- CoinManager.swift
+++ CoinManager.swift
@@ -5,10 +5,14 @@
 //  Created by Angela Yu on 11/09/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String,currency: String);
+    func didfailWithError(error: Error);
+}
 import Foundation
 
 struct CoinManager {
+    var delegate: CoinManagerDelegate?;
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
    // let apiKey = "YOUR_API_KEY_HERE"
@@ -20,6 +24,7 @@ struct CoinManager {
       //  let urlString = "\(baseURL)?\(apiKey)&q=\(currency)"
         let urlString = "\(baseURL)/\(currency)?apiKey=\(apiKey)";        self.performRequest(with: urlString);
     }
+    //Networking
     func performRequest(with urlString : String){
         if let url = URL(string: urlString){
             
@@ -27,15 +32,29 @@ struct CoinManager {
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil{
                     print(error!)
-                }
+                    return
+                };
+                if let safeData = data{
                //swift object into a string use string initialiser
                    let dataString = String(data: data!, encoding: .utf8);
-                   print(dataString);
+                   print(dataString!);
+                    //parse JSON data into real swift object
+                    self.parseJSON(currencyData: safeData)
                    }
-               
+            }
             task.resume();
                    
             }
             
+    }
+    func parseJSON(currencyData: Data){
+        let decoder = JSONDecoder();
+        do{
+            let decodedData = try decoder.decode(CurrencyData.self, from: currencyData);
+            print(decodedData.rate)
+        }
+        catch{
+            print(error)
+        }
     }
 }
---
+++ CurrencyData.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyData.swift
+//  ByteCoin
+//
+//  Created by user215496 on 7/13/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CurrencyData: Codable{
+    let rate: Double
+}
